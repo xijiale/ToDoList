cc5d5bf1447cf088279e27f5c2225bf4a38c9ae4
xijiale <772522327@qq.com>
2017-02-27 15:20:19 +0800

    js

--- ToDoList/list.js
+++ ToDoList/list.js
@@ -2,7 +2,25 @@
  * Created by Administrator on 2017/2/27.
  */
 //添加事件
+function addn() {
+    var x = document.getElementsByClassName("enter");
+    var y = document.getElementById("q1");
+    var div = document.createElement("div");
+    var put = document.createElement("input");
+    var sp = document.createElement("span");
+    var spa = document.createElement("span");
+    var nodesp = document.createTextNode(x.value);
+    sp.appendChild(nodesp);
 
+    var element = document.getElementById("task");
+    element.appendChild(div);
+    div.appendChild(put);
+    div.appendChild(sp);
+    div.appendChild(spa);
+    y.innerHTML++;
+    div.setAttribute(ClassName,inputtype);
+}
+addn();
 //统计事件
 //删除事件
 //勾选事件
