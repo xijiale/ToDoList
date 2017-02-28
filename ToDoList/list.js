bdfba358ad3f4e53827edc1dc80d83de7bc60875
xijiale <772522327@qq.com>
2017-02-28 13:57:40 +0800

    jQuery

--- ToDoList/list.js
+++ ToDoList/list.js
@@ -1,20 +1,28 @@
+//删除事件
 /**
  * Created by Administrator on 2017/2/27.
  */
 //添加事件
 $(document).ready(function () {
-    $("input [type='text']").change( function () {
-        var txt1 = $('<div></div>');
-        $(txt1).addClass("arr");
-        $('#arr1').after(txt1);
-        var txt2 = $("input [type='radio']");
-        var txt3 = $("<span></span>").text($('.enter').val());
-        $(txt1).append(txt2,txt3);
-
-
-
+    var todoLists = [];
+    $("#todoInput").change( function () {
+        var text  = $('.enter').val();
+        var htmlTempl = "<div class='arr'><input type='radio' class='inputtype'/><span>"+text+"</span> <i class='fa fa-minus-circle' aria-hidden='true' id='fa'></i></div>";
+        $('#task').append(htmlTempl);
+        todoLists.push(text);
+        $('#q1').text(todoLists.length);
+        // var txt1 = $('<div></div>');
+        // $(txt1).addClass("arr");
+        // var txt2 = $("input [type='radio']");
+        // var txt3 = $("<span></span>").text();
+        // $('#task').append(txt1);
+        // $(txt1).append(txt2,txt3);
+    });
+    $('#fa').on('click',function () {
+        $(this).parent().remove();
     });
 });
+//统计事件
 
 /*function addn() {
     var x = document.getElementsByClassName("enter");
@@ -35,12 +43,8 @@ $(document).ready(function () {
     div.setAttribute(ClassName,inputtype);
 }
 addn();*/
-//统计事件
-//删除事件
-$(document).ready(function () {
-    $('fa').click(function () {
-        $('arr').remove();
-    });
-})
+// $(document).ready(function () {
+//
+//     });
 //勾选事件
 //取消事件
